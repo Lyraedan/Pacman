01f93e1100dab25a6e66a696def54ebeab86bfed
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2023-03-03 21:32:52 +0000

    Sound priority

--- Pacman/AudioPlayer.cpp
+++ Pacman/AudioPlayer.cpp
@@ -2,26 +2,49 @@
 #include <iostream>
 #include <Windows.h>
 
-bool AudioPlayer::PlayWav(const char * filename)
+bool AudioPlayer::PlayWav(const char * filename, bool loop)
 {
+	if(loop)
+		return PlaySound(filename, NULL, SND_LOOP | SND_ASYNC);
+
 	return PlaySound(filename, NULL, SND_ASYNC);
 }
 
-bool AudioPlayer::PlayWavOnce(char * filename, float audioLength, float audioLengthMul)
+bool AudioPlayer::PlayWavOnce(char * filename, float audioLength, float audioLengthMul, int priority, bool loop)
 {
 	bool doPlay = false;
-	if (currentAudioFile != filename) {
-		currentAudioPosition = 0;
-		currentAudioFile = filename;
+	if (priority >= currentSoundPriority) {
+		currentSoundPriority = priority;
+
+		if (currentAudioFile != filename) {
+			currentAudioPosition = 0;
+			currentAudioFile = filename;
+			currentSoundLength = audioLength;
+			currentSoundMultiplier = audioLengthMul;
+		}
+
+		if (currentAudioPosition == 0) {
+			doPlay = PlayWav(currentAudioFile, loop);
+		}
 	}
 
-	if (currentAudioPosition == 0) {
-		doPlay = PlayWav(currentAudioFile);
-	}
 	currentAudioPosition++;
-	if (currentAudioPosition >= audioLength * audioLengthMul) {
+	if (currentAudioPosition >= currentSoundLength * currentSoundMultiplier) {
 		currentAudioPosition = 0;
+		currentSoundPriority = 0;
 		currentAudioFile = "";
+		currentSoundLength = 0;
+		currentSoundMultiplier = 0;
 	}
 	return true;
+}
+
+bool AudioPlayer::Stop()
+{
+	currentAudioPosition = 0;
+	currentSoundPriority = 0;
+	currentAudioFile = "";
+	currentSoundLength = 0;
+	currentSoundMultiplier = 0;
+	return PlaySound(NULL, NULL, 0);
 }
--- Pacman/AudioPlayer.h
+++ Pacman/AudioPlayer.h
@@ -1,9 +1,13 @@
 class AudioPlayer {
 	public:
-		bool PlayWav(const char* filename);
-		bool PlayWavOnce(char* filename, float audioLength, float audioLengthMul);
+		bool PlayWav(const char* filename, bool loop);
+		bool PlayWavOnce(char* filename, float audioLength, float audioLengthMul, int priority, bool loop);
+		bool Stop();
 
 	private:
 		float currentAudioPosition = 0;
+		int currentSoundPriority = 0;
+		float currentSoundLength = 0;
+		float currentSoundMultiplier = 0;
 		char* currentAudioFile = "";
 };
